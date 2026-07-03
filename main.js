c19264dfdcbd5f492058f0113daaa385447604c5
Mari Shankar S <marishankar.1603@gmail.com>
2026-07-03 20:28:23 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
+/*
+=========================================
+Project : Mari Shankar Portfolio
+File    : main.js
+=========================================
+*/
 
+import "./loader.js";
+import "./navbar.js";
+import "./typing.js";
+import "./observer.js";
+import "./particles.js";
+import "./cursor.js";
+import "./projects.js";
+import "./voice.js";
+import "./theme.js";
+import "./utils.js";
+
+console.log("🚀 Mari Shankar Portfolio Started");
